0bbfc439585e103f7daa9006dfcc610149484c33
liaofuxing <597429743@qq.com>
2020-10-21 16:41:35 +0800

    fix:  修改 store 中错误的注释

--- src/store/modules/routers.js
+++ src/store/modules/routers.js
@@ -16,13 +16,13 @@ const routers = {
   },
 
   actions: {
-    // 动态设置路由 通过commit state，参数为routerList
+    // 动态设置路由 通过commit state, 参数为routerList
     setRouterList({ commit }, routerList) {
       commit('set_router', constantRoutes.concat(routerList)) // 进行路由拼接并存储
     },
-    // 动态设置路由 通过commit state，参数为routerList
+    // 设置动态路由获取状态
     setRoutrerDataFlag({ commit }, routerDataFlag) {
-      commit('set_routrerDataFlag', routerDataFlag) // 进行路由拼接并存储
+      commit('set_routrerDataFlag', routerDataFlag)
     }
   }
 }
